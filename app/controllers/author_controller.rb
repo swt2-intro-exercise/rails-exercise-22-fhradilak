5262fe1f13a373ee7b6f8efad754491023a4397e
franziska.hradilak <franziska.hradilak@student.hpi.uni-potsdam.de>
2022-10-25 09:34:15 +0200

    add author model

--- app/controllers/author_controller.rb
+++ app/controllers/author_controller.rb
@@ -3,4 +3,11 @@ class AuthorController < ApplicationController
   def new
 
   end
+
+  def create
+    @author = Author.new(params[:author])
+
+    @author.save
+    redirect_to @author
+  end
 end
